9eca285274b145c6db5df7872ef6d27aa1f70ee6
Lenovo-PC\yuki <miura0808y@gmail.com>
2017-07-24 14:46:29 +0900

    クラスの作成の課題を提出します

---
+++ CampChallenge_java/013.クラス/Class1.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yuki
+ */
+
+public class Class1 {
+   
+    //パブリックな2つの変数を宣言
+    public String name = "";
+    public int age = 0; 
+    
+    //2つの変数に値を設定するパブリックなメソッド
+    public void setHuman(){
+        name = "田中";
+        age = 60;
+    }
+    //2つの変数をprintするパブリックなメソッド
+    public void printOut(){
+        System.out.print(name +"<br>" +age);
+    }
+    
+}
+
+
+
+
+
+
+
+
+    
+
