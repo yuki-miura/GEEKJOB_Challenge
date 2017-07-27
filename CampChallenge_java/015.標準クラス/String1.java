220d6bc52d278e4caaab7dd67e9438a146e0b8a3
EXP-0007\guest1Day <miura0808y@gmail.com>
2017-07-27 18:42:48 +0900

    バイト数と文字数の取得の課題を提出します

---
+++ CampChallenge_java/015.標準クラス/String1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+public class String1 {
+
+    public static void main(String[] args) {
+        String name = "みうらゆうき";
+        //nameのバイト数と文字長を表示
+        System.out.println(name.getBytes().length);
+        System.out.println(name.length());
+
+    }
+
+}
