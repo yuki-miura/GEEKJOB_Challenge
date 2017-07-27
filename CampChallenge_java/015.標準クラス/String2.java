bc794f4d796e8156c1dc914a9f05a760ebc72e8b
EXP-0007\guest1Day <miura0808y@gmail.com>
2017-07-27 18:43:14 +0900

    文字数の取得の課題を提出します

---
+++ CampChallenge_java/015.標準クラス/String2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+public class String2 {
+
+    public static void main(String[] args) {
+        //アドレスの＠以降を表示
+        String addresu = "miura0808y@gmail.com";
+        System.out.print(addresu.substring(10));
+
+    }
+}
